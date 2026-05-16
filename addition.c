444ea6cf9862013f6ea4307930e223ef9f9b99d5
Uttam Thapa <uttammadhu1022@gmail.com>
2026-05-16 12:56:16 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -7,7 +7,7 @@ printf("Program: Simple Addition of Two Numbers\n");
 printf("----------------------------------------\n");
 int a, b, sum;
 printf("Enter two numbers: ");
-scanf("%d", &a,&b);
+scanf("%d%d", &a,&b);
 sum = a + b;
 printf("Sum = %d\n", sum);
 return 0;
