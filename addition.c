624c878f69bfe24612b3da8c9dd6d3815cbf0f98
Uttam Thapa <uttammadhu1022@gmail.com>
2026-05-16 12:42:55 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main()
+{
+printf("Name: Uttam Thapa\n");
+printf("USN: 1JS25CI097\n");
+printf("Program: Simple Addition of Two Numbers\n");
+printf("----------------------------------------\n");
+int a, b, sum;
+printf("Enter two numbers: ");
+scanf("%d", &a,&b);
+sum = a + b;
+printf("Sum = %d\n", sum);
+return 0;
+}
